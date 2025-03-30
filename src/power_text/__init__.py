e023e0fe3bd631c2374869ef3d09050570a1b22c
xiaosuyyds <Xiaosu-1009@qq.com>
2025-03-30 15:11:17 +0800

    ready for 0.2.0

--- src/power_text/__init__.py
+++ src/power_text/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/xiaosuyyds/PowerText/blob/master/NOTICE
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .power_text import Font, draw_text
 
